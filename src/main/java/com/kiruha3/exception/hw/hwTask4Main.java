49530b97c33d00dbc25500db6c4c0800c0a36eb0
kirill <kiruha3@mail.ru>
2023-09-02 17:14:26 +0700

    поставил сеты где удобно, исправил циклы

--- src/main/java/com/kiruha3/exception/hw/hwTask4Main.java
+++ src/main/java/com/kiruha3/exception/hw/hwTask4Main.java
@@ -1,6 +1,9 @@
 package com.kiruha3.exception.hw;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class hwTask4Main {
 
@@ -12,17 +15,15 @@ public class hwTask4Main {
     private static void unicueWords(List<String> words) {
         Map<String, Integer> unicueWords = new HashMap<>();
         for (String word : words) {
-            unicueWords.put(word, 0);
-        }
-        for (String word : words) {
-            if (unicueWords.containsKey(word)) {
+            if (!unicueWords.containsKey(word)) {
+                unicueWords.put(word, 1);
+            } else {
                 unicueWords.put(word, unicueWords.get(word) + 1);
             }
-        }
-//        unicueWords.
-        System.out.println(unicueWords.values());
 
-    }
+        }
+        System.out.println(unicueWords.values());
+        unicueWords.entrySet();
 //    private static void countRepaterWords(Map<String, Integer> unicueWords, List<String> words) {
 //        for (int word = 0; word < words.size(); word++) {
 //            if (unicueWords.containsKey(words.get(word))) {
@@ -33,4 +34,5 @@ public class hwTask4Main {
 //    }
 
 
+    }
 }
